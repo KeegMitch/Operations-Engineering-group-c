e982baef179575074e0b0ad94fe015ece5362a1a
Crystalsawers <sawece1@student.op.ac.nz>
2024-05-14 11:06:43 +1200

    update borg and manual scripts

--- scripts/borg_mgmt.sh
+++ scripts/borg_mgmt.sh
@@ -6,24 +6,31 @@
 # Get the current date and time
 current_date=$(date +"%B_%d_%Y_%H%M%S")
 
-# create backup
+# create backup logs
+log_dir="$HOME/logs"
+log_file="$log_dir/borg_puppetconf.log"
 
-log_file="borg_puppetconf.log"
-
+if [ ! -d "$log_dir" ]; then
+    mkdir -p "$log_dir"
+fi
 
 sudo borg create --compression auto,lzma -e repokey-blake2 group-c@backup-c:/home/group-c/full_backups::puppetbackup_$current_date /etc/puppetlabs/ >> "$log_file" 2>&1  --stats
 
 # manual commands
-# sudo borg create --compression auto,lzma -e repokey-blake2 group-c@backup-c:/home/group-c/full_backups::puppetbackup_$(date +"%B_%d_%Y_%H%M%S") /etc/puppetlabs/ >> borg_puppetconf.log 2>&1 --stats
+# sudo borg create --compression auto,lzma -e repokey-blake2 group-c@backup-c:/home/group-c/full_backups::puppetbackup_$(date +"%B_%d_%Y_%H%M%S") /etc/puppetlabs/ --stats
 # sudo borg list group-c@backup-c:/home/group-c/full_backups
+# ssh setup
+
 
 # Capture the exit status of the borg create command
 borg_exit_status=$?
 
 # Check if the backup command exited successfully (exit code 0)
 if [ $borg_exit_status -eq 0 ]; then
+  echo "Check borg_puppetconf.log for output"
   echo "Backup completed successfully!" >> $log_file
 else
+  echo "Check borg_puppetconf.log for output"
   echo "Backup failed with exit code: $borg_exit_status" >> $log_file
 fi
 
--- scripts/puppet_backup.sh
+++ scripts/puppet_backup.sh
@@ -8,6 +8,14 @@ puppet_dir="/etc/puppetlabs/"
 # Get the current date and time
 current_date=$(date +"%B_%d_%Y_%H%M%S")
 
+# create backup
+log_dir="$HOME/logs"
+log_file="$log_dir/manual_puppetconf.log"
+
+if [ ! -d "$log_dir" ]; then
+    mkdir -p "$log_dir"
+fi
+
 # Define archive filename
 archive_name="puppetbackup_$current_date.tar.gz"
 
@@ -17,22 +25,29 @@ backup_dir="$HOME/mgmt_backups"
 if [ ! -d "$backup_dir" ]; then
     # Create the directory
     sudo mkdir -p "$backup_dir"
-    echo "Directory $backup_dir created."
+	echo "Check manual_puppetconf.log for output"
+    echo "Directory $backup_dir created." >> $log_file
 else
-    echo "Directory $backup_dir already exists. Backing up puppet config ..."
+	echo "Check manual_puppetconf.log for output"
+    echo "Directory $backup_dir already exists. Backing up puppet config ..." >> $log_file
 fi
 
 # Create a compressed tar archive directly from source files
 # sudo tar -czvf "$HOME/mgmt_backups/$archive_name" "$modules_dir" "$site_pp"
-sudo tar -czvf "$backup_dir/$archive_name" "$puppet_dir"
+sudo tar -czvf "$backup_dir/$archive_name" "$puppet_dir" >> $log_file
+
+exit_status=$?
 
 if [[ $? -eq 0 ]]; then
-echo "Backup created: $backup_dir/$archive_name"
-
+echo "Backup created: Check manual_puppetconf.log for output"
+echo "Backup created: $backup_dir/$archive_name" >> $log_file
+else
+  echo "Backup failed with exit code: $exit_status , Check manual_puppetconf.log for output"
+  echo "Backup failed with exit code: $exit_status" >> $log_file
 fi
 
-# automated by the sudo crontab as well that runs every day at 6pm NZT / 6am UTC
+# automated by the sudo crontab as well that runs every day at 7pm NZT / 7am UTC (cron changed to NZT)
 
 # following commands in sudo crontab:
 # HOME=/home/group-c
-# 0 6 * * * home/group-c/puppet_backup.sh > logfile.log
+# 0 19 * * * home/group-c/puppet_backup.sh > /logs/cron.log
