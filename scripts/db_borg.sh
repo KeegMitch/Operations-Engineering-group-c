40e491c42e125d93c77c74521572ee04ae2f0be7
Crystalsawers <sawece1@student.op.ac.nz>
2024-05-14 11:21:51 +1200

    edit borg db script

--- scripts/db_borg.sh
+++ scripts/db_borg.sh
@@ -8,14 +8,14 @@ echo
 current_date=$(date +"%B_%d_%Y_%H%M%S")
 
 # Backup ownCloud database
-sudo mysqldump -e -p"$mysql_password" owncloud > owncloud_backup.sql
+sudo mysqldump -e -p"$mysql_password" owncloud > dbowncloud_backup.sql
 
 # Borg backup
-sudo borg create --compression auto,lzma -e repokey-blake2 group-c@backup-c:/home/group-c/full_backups::owncloud_backup_$current_date owncloud_backup.sql --stats
+sudo borg create --compression auto,lzma -e repokey-blake2 group-c@backup-c:/home/group-c/full_backups::dbowncloud_backup_$current_date dbowncloud_backup.sql --stats
 
 # Remove local backup file if necessary
 # sudo rm owncloud_backup.sql
 
 
-# sudo mysqldump -e -p owncloud > owncloud_backup.sql
+# sudo mysqldump -e -p owncloud > dbowncloud_backup.sql
 # sudo mysqldump -e -p --all-databases > db_backupv2.sql
