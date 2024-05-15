40fe5c27a13cd3e8c071ca6b7f448dfed9c42085
Crystal Sawers <sawece1@student.op.ac.nz>
2024-05-15 14:03:11 +1200

    update db script

--- scripts/db_backup.sh
+++ scripts/db_backup.sh
@@ -6,7 +6,7 @@ echo
 
 # Server credentials
 remote_username="group-c"
-remote_server="10.2.0.5" # you can also use the hostname "backup-c"
+remote_server="backup-c" # you can also use the private ip 
 remote_directory="~/"
 
 # Get list of databases
@@ -17,8 +17,12 @@ for db in $databases; do
     # Create backup file
     sudo mysqldump "$db" > "$db.sql"
     
-    # Transfer backup file to remote server using SCP
-    sudo scp "$db.sql" "$remote_username"@"$remote_server":"$remote_directory"
+
+ # Transfer backup file to remote server using rsync
+	sudo rsync -av -e "ssh -i /home/group-c/.ssh/id_rsa" "~/$db.sql" "$remote_username"@"$remote_server":"$remote_directory"
+	
+	# sudo rsync -av -e "ssh -i /home/group-c/.ssh/id_rsa" "~/owncloud.sql" group-c@backup-c:~/
+	# sudo rsync -av ~/owncloud.sql group-c@backup-c:~/
     
     # Remove local backup file
     sudo rm "$db.sql"
