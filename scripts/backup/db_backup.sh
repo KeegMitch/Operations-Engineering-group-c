92f37cf134e06d6e828fd9c4455fd9c320026865
Crystalsawers <sawece1@student.op.ac.nz>
2024-06-04 05:26:35 +1200

    update

--- scripts/backup/db_backup.sh
+++ scripts/backup/db_backup.sh
@@ -17,7 +17,6 @@ fi
 
 log_file="dbbackup_log.log"
 
-
 # Server credentials
 remote_username="group-c"
 backup_server="10.2.0.5" # private ip of backup-c
@@ -26,11 +25,8 @@ backup_host="backup-c"
 storage_server="20.211.153.89"
 storage_host="offsite"
 
-
-
 remote_directory="/home/$remote_username/database_backup"
 storage_directory="/home/$remote_username/db-c"
-
 
 # Get list of databases
 databases=$(sudo mysql -e "SHOW DATABASES;" | grep -Ev "(Database|information_schema|performance_schema|mysql)")
@@ -38,7 +34,7 @@ databases=$(sudo mysql -e "SHOW DATABASES;" | grep -Ev "(Database|information_sc
 # Function to handle errors
 handle_error() {
     echo "Error: $1"
-        echo "$(date +"%Y-%m-%d %H:%M:%S") - Error: $1" >> "$log_file"
+    echo "$(date +"%Y-%m-%d %H:%M:%S") - Error: $1" >> "$log_file"
     exit 1
 }
 
@@ -50,28 +46,23 @@ log_message() {
 # Initialize log file
 echo "Backup Log $(date +"%Y-%m-%d %H:%M:%S")" > "$log_file"
 
-
 # Iterate over each database and create a backup file
 for db in $databases; do
+    # Create backup file with timestamp
+    timestamp=$(date +"%Y%m%d_%H%M%S")
+    backup_file="$HOME/$db_$timestamp.sql"
 
- # Create backup file
-    sudo mysqldump "$db" -p"$mysql_password" > "$HOME/$db.sql" || handle_error "Failed to create MySQL dump for database $db"
-    log_message "Created MySQL dump for database $db"
+    # Create backup file
+    sudo mysqldump "$db" -p"$mysql_password" > "$backup_file" || handle_error "Failed to create MySQL dump for database $db"
+    log_message "Created MySQL dump for database $db: $backup_file"
 
     # Rsync to STORAGE server
-    if ! sudo rsync -av -e "ssh -i /home/group-c/.ssh/id_rsa_db_storage" "$HOME/$db.sql" "$remote_username@$storage_server:$storage_directory/"; then
+    if ! sudo rsync -av -e "ssh -i /home/group-c/.ssh/id_rsa_db_storage" "$backup_file" "$remote_username@$storage_server:$storage_directory/"; then
         handle_error "Failed to rsync backup to $storage_host"
     fi
-    log_message "Rsynced backup to $storage_host"
-
+    log_message "Rsynced backup to $storage_host: $backup_file"
 done
 
 echo "Backup process completed successfully"
 log_message "Backup process completed successfully"
 
-# Manual testing commands
-# sudo rsync -av -e "ssh -i /home/group-c/.ssh/id_rsa_db_1" "$HOME/owncloud.sql" group-c@backup-c:~
-# sudo rsync -av -e "ssh -i /home/group-c/.ssh/id_rsa_db_storage" "$HOME/owncloud.sql" group-c@20.211.153.89:~/db-c
-
-# sudo mysqldump -e -p owncloud > owncloud_backup.sql
-# sudo mysqldump -e -p --all-databases > db_backupv2.sql
